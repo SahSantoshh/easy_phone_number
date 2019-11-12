44d2b7a89c4e1f6d56a4fea032e95c9fd1af92e3
SahSantoshh <santosh101410@gmail.com>
2019-11-12 13:54:56 +0545

    find your associated words for your number

---
+++ lib/phone_to_word.rb
@@ -0,0 +1,123 @@
+# frozen_string_literal: true
+
+require 'json'
+
+# Exceptions related to phone number
+class InvalidPhoneError < StandardError; end
+
+# class responsible to find probable word for any phone number
+class PhoneToWord
+  attr_reader :phone_number
+
+  DICTIONARY_FILE_PATH = 'public/dictionary.txt'
+  DICTIONARY_JSON_FILE_PATH = 'public/dictionary.json'
+  MAX_PHONE_NUMBER_LENGTH = 10
+  MIN_WORD_LENGTH = 3
+  MAX_WORD_COMBINATION = 3
+
+  def initialize(phone_number = nil)
+    @phone_number = phone_number.to_s
+
+    @words_list_hash = Hash.new { |h, k| h[k] = [] }
+
+    # save words with it's assicated number in a json file
+    if !File.file?(DICTIONARY_JSON_FILE_PATH)
+      File.read(DICTIONARY_FILE_PATH).split("\n").each do |word|
+        word_length = word.length
+        if word_length > MAX_PHONE_NUMBER_LENGTH || word_length < MIN_WORD_LENGTH
+          next
+        end
+
+        word = word.downcase
+        number = get_number_of_word(word)
+        @words_list_hash[number] << word
+      end
+      File.open(DICTIONARY_JSON_FILE_PATH, 'w') { |file| file.write(@words_list_hash.to_json) }
+    else
+      @words_list_hash = JSON.parse(File.read(DICTIONARY_JSON_FILE_PATH))
+    end
+
+    # Ask user for input
+    if @phone_number.empty?
+      puts 'Please enter 10 digit phone number:'
+      @phone_number = STDIN.gets.chomp
+    end
+
+    # Raise custom exception on invalid phone number
+    raise InvalidPhoneError unless valid_phone_number?
+  end
+
+  # will return false if phone number invalid
+  # number max length is MAX_PHONE_NUMBER_LENGTH = 10
+  # number should not contain 0s and 1s
+  def valid_phone_number?
+    !@phone_number.empty? && @phone_number.length == MAX_PHONE_NUMBER_LENGTH &&
+      @phone_number.match(/^[2-9]*$/)
+  end
+
+  # Get associated number for the word
+  def get_number_of_word(word)
+    number_for_word = ''
+    word.split('').each do |a_letter|
+      case a_letter
+      when 'a', 'b', 'c' then      number_for_word += '2'
+      when 'd', 'e', 'f' then      number_for_word += '3'
+      when 'g', 'h', 'i' then      number_for_word += '4'
+      when 'j', 'k', 'l' then      number_for_word += '5'
+      when 'm', 'n', 'o' then      number_for_word += '6'
+      when 'p', 'q', 'r', 's' then number_for_word += '7'
+      when 't', 'u', 'v' then      number_for_word += '8'
+      when 'w', 'x', 'y', 'z' then number_for_word += '9'
+      end
+    end
+    number_for_word
+  end
+
+  # 10, 5 x 5
+  # 4 x 6, 6 x 4
+  # 3 x 3 x 4,
+  # 3 x 4 x 3,
+  # 4 x 3 x 3
+  # 3 x 7, 7 x 3
+  def possible_combinations
+    with_3_digit = @phone_number[0..2]
+    with_4_digit = @phone_number[0..3]
+    with_6_digit = @phone_number[6..9]
+    with_7_digit = @phone_number[7..9]
+    [
+      [@phone_number],
+      [@phone_number[0..4], @phone_number[5..9]],
+      [with_4_digit, @phone_number[4..9]],
+      [@phone_number[0..5], with_6_digit],
+      [with_3_digit, @phone_number[3..5], with_6_digit],
+      [with_3_digit, @phone_number[3..6], with_7_digit],
+      [with_4_digit, @phone_number[4..6], with_7_digit],
+      [with_3_digit, @phone_number[3..9]],
+      [@phone_number[0..6], with_7_digit]
+    ]
+  end
+
+  # all combinations of words for the phone number
+  def possilbe_words_combinations
+    all_words_combinations = []
+    possible_combinations.each do |a_combination|
+      words_for_a_combination = []
+      a_combination.each do |a_number|
+        word = @words_list_hash[a_number]
+        words_for_a_combination << word if word && !word.empty?
+      end
+      if a_combination.size == words_for_a_combination.size
+        all_words_combinations << words_for_a_combination
+      end
+    end
+
+    all_words = []
+    all_words_combinations.each do |one_combination|
+      next if one_combination.size > MAX_WORD_COMBINATION
+
+      final_combination, *sub_combination = one_combination
+      all_words += final_combination.product(*sub_combination)
+    end
+    all_words
+  end
+end
---
+++ spec/phone_to_word_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'PhoneToWord' do
+  describe 'initialize' do
+    context 'invalid phone number with 0s and 1s' do
+      it 'should through InvaliPhoneNumber exception' do
+        expect { PhoneToWord.new(1_081_787_825) }.to raise_error(InvalidPhoneError)
+      end
+    end
+
+    context 'invalid phone number with mismatch length' do
+      it 'should through InvaliPhoneNumber exception for number with more than 10 digits' do
+        expect { PhoneToWord.new(234_581_787_825) }.to raise_error(InvalidPhoneError)
+      end
+
+      it 'should through InvaliPhoneNumber exception for number with less than 10 digits' do
+        expect { PhoneToWord.new(81_787_825) }.to raise_error(InvalidPhoneError)
+      end
+    end
+
+    context 'valid phone number with length 10, no 0s and 1a' do
+      it 'should through InvaliPhoneNumber exception' do
+        expect { PhoneToWord.new(2_345_678_922) }.not_to raise_error(InvalidPhoneError)
+      end
+    end
+  end
+
+  describe 'all combinations of phone number for valid phone number' do
+    it 'should get all possible combinations of the phone number' do
+      combinations = PhoneToWord.new(6_686_787_825).possible_combinations
+      excepted_array = [%w[668 678 7825], %w[668 6787 825], %w[668 6787825],
+                        %w[6686 787 825], %w[6686 787825], %w[66867 87825],
+                        %w[668678 7825], %w[6686787 825], ['6686787825']]
+      expect(combinations).to match_array(excepted_array)
+    end
+  end
+
+  describe 'all possible combinations of word for valid phone number' do
+    it 'should list all possible words combinations' do
+      words_combinations = PhoneToWord.new(6_686_787_825).possilbe_words_combinations
+      excepted_array = [['motortruck'], %w[noun struck], %w[onto struck],
+                        %w[motor truck], %w[motor usual], %w[nouns truck],
+                        %w[nouns usual], %w[mot opt puck], %w[mot opt ruck],
+                        %w[mot opt suck], %w[mot opts taj], %w[oot ort puck],
+                        %w[oot ort ruck], %w[oot ort suck], %w[oot orts taj],
+                        %w[mot opus taj], %w[mot ort puck], %w[mot ort ruck],
+                        %w[mot ort suck], %w[oot opt suck], %w[oot opts taj],
+                        %w[oot opus taj], %w[mot orts taj], %w[not opt puck],
+                        %w[not opt ruck], %w[not opt suck], %w[onto sup taj],
+                        %w[onto suq taj], %w[oot opt puck], %w[oot opt ruck],
+                        %w[not opts taj], %w[not opus taj], %w[not ort puck],
+                        %w[not ort ruck], %w[not ort suck], %w[not orts taj],
+                        %w[noun pup taj], %w[noun pur taj], %w[noun suq taj],
+                        %w[onto pup taj], %w[onto pur taj], %w[onto pus taj],
+                        %w[noun pus taj], %w[noun sup taj]]
+      expect(words_combinations).to match_array(excepted_array)
+    end
+  end
+end
